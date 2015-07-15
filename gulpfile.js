ada62b24ba67498c7a079eb136abe6f8c047a856
Charles Bido <charlesbido@me.com>
2015-07-14 22:07:18 -0500

    restructuring of codebase

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,33 @@
 var gulp = require("gulp");
-var connect = require("gulp-connect");
-var sass = require('gulp-sass');
-
-/**
- * This task will create a livereload server.
- * http://localhost:3000
- */
+var connect = require('gulp-connect');
 gulp.task('connect', function() {
   connect.server({
-    livereload: true,
-    port: 3000,
+    root:'./app',
+    livereload: true
   });
 });
 
 gulp.task('html', function() {
-  gulp.src('./*.html')
+  gulp.src('./app/*.html')
     .pipe(connect.reload());
 });
 
 gulp.task('sass', function() {
-  gulp.src('./sass/**/*.scss')
+  gulp.src('./app/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest('./app/css/app.css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+  gulp.src('./app/js/**/*.js')
     .pipe(connect.reload());
 });
 
 gulp.task('watch', function() {
-  gulp.watch(['./*.html'], ['html']);
-  gulp.watch(['./**/*.scss'], ['sass']);
+  gulp.watch(['./app/*.html'], ['html']);
+  gulp.watch(['./app/sass/**/*.scss'], ['scss']);
+  gulp.watch(['./app/js/**/*.js'], ['js']);
 });
 
 gulp.task('default', ['connect', 'watch']);
